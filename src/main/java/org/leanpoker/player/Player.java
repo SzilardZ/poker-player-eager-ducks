a6dfc2daefc4d379c8a31660a843522c6321969e
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-24 10:53:00 +0200

    first try

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
@@ -9,6 +11,9 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        JsonParser parser = new JsonParser();
+        JsonObject o = parser.parse("{\"a\": \"A\"}").getAsJsonObject();
+        System.out.println(o);
         return 0;
     }
 

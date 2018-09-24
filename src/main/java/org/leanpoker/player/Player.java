5241454da1ad0ebfc66ba364413e7fd73ffb79b8
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-24 12:20:06 +0200

    try to log players and rounds and gameId

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,6 +13,8 @@ public class Player {
         JsonObject root = request.getAsJsonObject();
         JsonArray players = root.getAsJsonArray("players");
         System.err.println("Players: " + players);
+        JsonPrimitive gameId = root.getAsJsonPrimitive("game_id");
+        System.err.println("GameId : " + gameId);
         JsonPrimitive rounds = root.getAsJsonPrimitive("round");
         System.err.println("Rounds: HUHU" + rounds);
 

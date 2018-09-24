ceedb54a9cef109defaff3341c1ca383899452c7
laszloOrha <orha.laszlo.93@gmail.com>
2018-09-24 14:29:48 +0200

    sANYI

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -5,6 +5,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
+import java.util.HashMap;
+
 public class Player {
 
     static final String VERSION = "Default Java folding player";
@@ -12,13 +14,16 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject root = request.getAsJsonObject();
         JsonArray players = root.getAsJsonArray("players");
-        System.err.println("Players: " + players);
-        JsonPrimitive gameId = root.getAsJsonPrimitive("game_id");
-        System.err.println("GameId : " + gameId);
-        JsonPrimitive rounds = root.getAsJsonPrimitive("round");
-        System.err.println("Rounds: HUHU" + rounds);
+//        System.err.println("Players: " + players);
+        JsonObject thisPlayer = players.getAsJsonObject();
+        JsonArray ourCards = thisPlayer.getAsJsonArray("hole_cards");
+        System.err.println("SANYI: " + ourCards);
+//        JsonPrimitive gameId = root.getAsJsonPrimitive("game_id");
+//        System.err.println("GameId : " + gameId);
+//        JsonPrimitive rounds = root.getAsJsonPrimitive("round");
+//        System.err.println("Rounds: HUHU" + rounds);
         JsonArray communityCards = root.getAsJsonArray("community_cards");
-        System.err.println("COMMUNITY: " + communityCards);
+//        System.err.println("COMMUNITY: " + communityCards);
 
         return 0;
     }
